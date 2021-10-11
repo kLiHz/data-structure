55f8c02deb3140683a399f8ea537d82a0b37af0a
henry_23 <henry.maoht@outlook.com>
2021-10-11 12:21:52 +0800

    fix(shared-stack): const modifier, prettier output

--- stack/shared-stack-demo.cpp
+++ stack/shared-stack-demo.cpp
@@ -38,15 +38,15 @@ public:
         }
     }
 
-    auto full() {
+    auto full() const {
         return (top_pos[0] - 1) == top_pos[1];
     }
 
-    auto empty() {
+    auto empty() const {
         return (top_pos[0] == 0) && (top_pos[1] == len - 1);
     }
 
-    auto empty(GrowthDirection dir) {
+    auto empty(GrowthDirection dir) const {
         return dir == GrowthDirection::UP ?
             (top_pos[0] == 0) :
             (top_pos[1] == len - 1);
@@ -74,13 +74,24 @@ public:
         return true;
     };
 
-    auto top(GrowthDirection dir) {
+    auto top(GrowthDirection dir) const {
         if (dir == GrowthDirection::UP) {
             return ptr[top_pos[0]-1];
         } else {
             return ptr[top_pos[1]+1];
         }
     };
+    
+    auto size(GrowthDirection dir) const {
+        if (dir == GrowthDirection::UP) {
+            return top_pos[0];
+        } else {
+            return len - (top_pos[1] + 1);
+        }
+    };
+
+    auto capacity() const { return len; }
+    auto data() const { return ptr; }
 };
 
 template<typename T>
@@ -103,26 +114,76 @@ public:
     auto push(const T& val) {
         return mem.push(d, val);
     };
-    auto empty() {
+    auto empty() const {
         return mem.empty(d);
     }
-    auto full() {
+    auto full() const {
         return mem.full();
     }
 };
 
+#include <iostream>
+#include <string>
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, BiStackMemory<T> const & mem) {
+    auto x1 = mem.size(BiStackMemory<T>::GrowthDirection::UP);
+    auto x2 = mem.size(BiStackMemory<T>::GrowthDirection::DOWN);
+    std::string data_line;
+    std::string indicator_line;
+    auto len = mem.capacity();
+    for (int i = 0; i < len; i++) {
+        auto val = std::to_string(mem.data()[i]);
+        data_line += val + ' ';
+        auto blank = std::string(val.length() + 1, ' ');
+        if (x1 - 1 == i || len - x2 == i) {
+            blank[0] = '^';
+        }
+        indicator_line += blank;
+    }
+    os << data_line << "\n" << indicator_line;
+    return os;
+}
+
 int main() {
     BiStackMemory<int> mem(10);
     SharedStack<int> s1(mem), s2(mem);
-    for (int i = 1; i < 10; i += 2) {
-        s1.push(i);
-    }
-    for (int i = 2; i <= 10; i += 2) {
-        s2.push(i);
-    }
+
+    std::cout << "Memory allocated.\n";
+    std::cout << std::boolalpha;
+    std::cout << "s1.empty(): " << s1.empty() << "\n";
+    std::cout << "s2.empty(): " << s2.empty() << "\n\n";
+
+    for (int i = 1; i < 10; i += 2) { s1.push(i); }
+    std::cout << mem << "\n";
+    std::cout << "s1.empty(): " << s1.empty() << "\n";
+    std::cout << "s2.empty(): " << s2.empty() << "\n\n";
+    
+    for (int i = 2; i <= 10; i += 2) { s2.push(i); }
+    std::cout << mem << "\n";
+    std::cout << "s1.empty(): " << s1.empty() << "\n";
+    std::cout << "s2.empty(): " << s2.empty() << "\n";
+    std::cout << "s1.full(): " << s1.full() << "\n";
+    std::cout << "s2.full(): " << s2.full() << "\n\n";
+
+    std::cout << "s1.pop():\n";
     s1.pop();
+    std::cout << mem << "\n";
+    std::cout << "s1.empty(): " << s1.empty() << "\n";
+    std::cout << "s2.empty(): " << s2.empty() << "\n";
+    std::cout << "s1.full(): " << s1.full() << "\n";
+    std::cout << "s2.full(): " << s2.full() << "\n\n";
+
+    std::cout << "s2.pop():\n";
     s2.pop();
+    std::cout << mem << "\n";
+
+    std::cout << "s1.pop():\n";
     s1.pop();
+    std::cout << mem << "\n";
+
+    std::cout << "s2.pop():\n";
     s2.pop();
+    std::cout << mem << "\n";
 }
 
